2adf1663cde7c42aba89075361a04418c2591721
Aleksey Abramov <dev.abramoff@gmail.com>
2020-04-03 11:32:22 +0300

    added pages: main, category, product, cart, checkout, contact

---
+++ resources/views/header.blade.php
@@ -0,0 +1,38 @@
+<!-- Header section -->
+<header class="header-section">
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 text-center text-lg-left">
+                    <!-- logo -->
+                    <a href="{{ route('page.main') }}" class="site-logo">
+                        <img src="{{ asset('assets/img/logo.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="col-xl-6 col-lg-5">
+                    <form class="header-search-form">
+                        <input type="text" placeholder="Search on divisima ....">
+                        <button><i class="flaticon-search"></i></button>
+                    </form>
+                </div>
+                <div class="col-xl-4 col-lg-5">
+                    <div class="user-panel">
+                        <div class="up-item">
+                            <i class="flaticon-profile"></i>
+                            <a href="#">Sign</a> In or <a href="#">Create Account</a>
+                        </div>
+                        <div class="up-item">
+                            <div class="shopping-card">
+                                <i class="flaticon-bag"></i>
+                                <span>0</span>
+                            </div>
+                            <a href="{{ route('page.cart') }}">Shopping Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('partials.top-menu')
+</header>
+<!-- Header section end -->
