a8bc523d7aedb61a89b7944d800e11fb4a8fade5
Alex Veikin <vejkin@yandex.ru>
2020-04-04 10:54:15 +0300

    Added tailwind css framework.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,9 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 if (mix.inProduction()) {
   mix.version();
 }
-
-mix.options({
-  // extractVueStyles: false,
-  // processCssUrls: true,
-  // uglify: {},
-  // purifyCss: false,
-  // purifyCss: {},
-  postCss: [require('autoprefixer')],
-  // clearConsole: false
-});
 
 mix.webpackConfig({
   resolve: {
@@ -31,6 +22,19 @@ mix
   // admin
   .js('resources/js/admin/admin.js', 'public/js/admin')
   .sass('resources/sass/admin/admin.scss', 'public/css/admin')
+
+  .options({
+    processCssUrls: false,
+    postCss: [
+      tailwindcss('resources/js/tailwind.config.js'),
+      require('autoprefixer')
+    ],
+    // extractVueStyles: false,
+    // uglify: {},
+    purifyCss: true,
+    // purifyCss: {},
+    // clearConsole: false
+  })
 
   .browserSync({
     proxy: process.env.MIX_APP_URL.split('://')[1],
